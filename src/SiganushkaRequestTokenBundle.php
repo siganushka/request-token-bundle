4f5ab21cf25d71951893d85721ce83c72e467077
siganushka <siganushka@gmail.com>
2021-10-27 17:03:47 +0800

    fix: fix missing bundle entry file.

---
+++ src/SiganushkaRequestTokenBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\RequestTokenBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SiganushkaRequestTokenBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+    }
+}
