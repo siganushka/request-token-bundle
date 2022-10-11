8c7b87b5115333ef8c3831ecbec4b7ddd9db2ef4
siganushka <siganushka@gmail.com>
2022-10-11 19:16:51 +0800

    fix: fix psalm error on PHP8.1

--- src/Generator/UuidTokenGenerator.php
+++ src/Generator/UuidTokenGenerator.php
@@ -11,15 +11,15 @@ use Symfony\Component\Uid\Uuid;
  */
 class UuidTokenGenerator implements RequestTokenGeneratorInterface
 {
-    public function __construct()
+    /**
+     * @psalm-suppress UndefinedClass
+     */
+    public function generate(): string
     {
         if (!class_exists(Uuid::class)) {
             throw new \LogicException(sprintf('The "%s" class requires the "Uid" component. Try running "composer require symfony/uid".', self::class));
         }
-    }
 
-    public function generate(): string
-    {
         return (string) Uuid::v1();
     }
 }
