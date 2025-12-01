be1233de7c539de4e59c006eb05fb9195d66a184
siganushka <siganushka@gmail.com>
2025-12-01 10:58:00 +0800

    tests: Update actions/checkout to v6

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,20 +6,20 @@ namespace Siganushka\RequestTokenBundle\DependencyInjection;
 
 use Siganushka\RequestTokenBundle\Generator\RequestTokenGeneratorInterface;
 use Siganushka\RequestTokenBundle\Generator\UniqidTokenGenerator;
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    /**
+     * @return TreeBuilder<'array'>
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('siganushka_request_token');
-        /** @var ArrayNodeDefinition */
         $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
-            // ->canBeEnabled()
             ->children()
                 ->booleanNode('enabled')
                     ->defaultFalse()
